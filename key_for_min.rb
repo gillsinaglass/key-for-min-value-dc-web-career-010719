8a3299a817df0c08c5cd15cbb90f59199462adce
Ryan Gill <rdgill95@gmail.com>
2019-01-04 00:26:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,10 @@
 
 def key_for_min_value(name_hash)
   low_val_key = nil
-   low_val = 10000
+  low_val = 10000
  if name_hash == {}
    nil
  else
-   low_val_key = nil
-   low_val = 10000
    name_hash.each do |key, value|
      if value < low_val
        low_val = value
@@ -17,5 +15,4 @@ def key_for_min_value(name_hash)
    end
    low_val_key
  end
- key_for_min_value(name_hash)
 end
